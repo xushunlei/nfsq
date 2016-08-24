a649708a02b65d5d28531892f15f64ff6abbc45b
xushunlei <123834788@qq.com>
2016-08-24 09:47:09 +0800

    Update TestDao.java

--- BooksSysT/test/testfuc/TestDao.java
+++ BooksSysT/test/testfuc/TestDao.java
@@ -3,7 +3,7 @@ package testfuc;
 public class TestDao {
 public static void main(String[] args) {
 	System.out.println("hello world!good moning");
-	int age=11;
+	int age=10;
 	System.out.println(age);
 }
 }
