7234148ea31996f78ba994cd22951dd4c3aa0948
xsl <123834788@qq.com>
2016-08-24 09:43:18 +0800

    14

--- BooksSysT/test/testfuc/TestDao.java
+++ BooksSysT/test/testfuc/TestDao.java
@@ -3,7 +3,7 @@ package testfuc;
 public class TestDao {
 public static void main(String[] args) {
 	System.out.println("hello world!good moning");
-	int age=18;
+	int age=14;
 	System.out.println(age);
 }
 }
